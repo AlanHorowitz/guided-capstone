fb9c41b04515ce6c8fcec286b3f3c2747b922f5d
AlanHorowitz <alanhorowitz63@gmail.com>
2021-06-21 17:01:14 -0400

    save changes

--- step3.py
+++ step3.py
@@ -1,14 +1,26 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, count, desc, countDistinct
+import pyspark.sql.functions as f
+
+
+def apply_latest(df):
+    return df
+
 
 spark = SparkSession.builder.master('local').appName('app').getOrCreate()
-trade_common = spark.read.parquet("output_dir/partition=Q")
+trade_common = spark.read.parquet("output_dir/partition=T")
+
+trade = trade_common.select("trade_dt", "symbol", "exchange", "event_tm", "event_seq_nb", "arrival_tm", "trade_pr")
+
+trade.createOrReplaceTempView('trade_v')
+r = spark.sql("select trade_dt from trade_v")
+r.show(5)
+trade_corrected = apply_latest(trade)
 
 trade_common.show(5, False)
 print(trade_common.count())
 trade_common.printSchema()
 
-trade_common.groupBy(col('trade_dt'), col('symbol'), col('exchange'), col('event_tm'))\
-    .agg(count('arrival_tm').alias('arr')).orderBy(desc('arr')).show(10)
+gr = trade_common.groupBy(col('trade_dt'), col('symbol'), col('exchange'), col('event_tm'))
 
-trade_common.select(countDistinct(col('event_tm'))).show()
+gr.agg(f.max('arrival_tm').alias('arrival_tm'), f.avg("ask_pr")).show(5)
