772b53fbef632bd718cb6cedb8909d57a9f55d9c
AlanHorowitz <alanhorowitz63@gmail.com>
2021-06-22 17:33:04 -0400

    more changes

--- step3.py
+++ step3.py
@@ -13,19 +13,20 @@ def apply_latest(df):
         .drop(f.col('tmp'))
 
 
-spark = SparkSession.builder.master('local').appName('app').getOrCreate()
+if __name__ == "__main__":
+    spark = SparkSession.builder.master('local').appName('app').getOrCreate()
 
-trade_common_df = spark.read.parquet("output_dir/partition=T")
-trade_df = trade_common_df.select("trade_dt", "symbol", "exchange", "event_tm", "event_seq_nb",
-                                  "arrival_tm", "trade_pr")
-quote_common_df = spark.read.parquet("output_dir/partition=Q")
-quote_df = trade_common_df.select("trade_dt", "symbol", "exchange", "event_tm", "event_seq_nb",
-                                  "arrival_tm", "bid_pr", "bid_size", "ask_pr", "ask_size")
+    trade_common_df = spark.read.parquet("output_dir/partition=T")
+    trade_df = trade_common_df.select("trade_dt", "symbol", "exchange", "event_tm", "event_seq_nb",
+                                      "arrival_tm", "trade_pr")
+    quote_common_df = spark.read.parquet("output_dir/partition=Q")
+    quote_df = trade_common_df.select("trade_dt", "symbol", "exchange", "event_tm", "event_seq_nb",
+                                      "arrival_tm", "bid_pr", "bid_size", "ask_pr", "ask_size")
 
-dataframes = {'trade': trade_df,
-              'quote': quote_df}
+    dataframes = {'trade': trade_df,
+                  'quote': quote_df}
 
-for df_name in dataframes:
-    cloud_storage_path = f'wasbs://test@guidedcapstonesa.blob.core.windows.net'
-    apply_latest(dataframes[df_name]) \
-        .write.mode('overwrite').parquet(f"{cloud_storage_path}/{df_name}/{df_name}_dt={date.today()}")
+    for df_name in dataframes:
+        cloud_storage_path = f'wasbs://test@guidedcapstonesa.blob.core.windows.net'
+        apply_latest(dataframes[df_name]) \
+            .write.mode('overwrite').parquet(f"{cloud_storage_path}/{df_name}/{df_name}_dt={date.today()}")
--- step3_test.py
+++ step3_test.py
@@ -1,3 +1,47 @@
-import pytest
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField
+import pyspark.sql.types as T
+from datetime import date, datetime
+from decimal import Decimal
+from step3 import apply_latest
+
+TEST_SCHEMA = StructType([StructField("trade_dt", T.DateType(), False),
+                          StructField("symbol", T.StringType(), False),
+                          StructField("exchange", T.StringType(), False),
+                          StructField("event_tm", T.TimestampType(), False),
+                          StructField("event_seq_nb", T.IntegerType(), False),
+                          StructField("arrival_tm", T.TimestampType(), False),
+                          StructField("trade_pr", T.DecimalType(10, 2), True)]
+                         )
 
 
+def test_apply_latest():
+    spark = SparkSession.builder.master('local').appName('app').getOrCreate()
+
+    records = ((date(2021, 6, 1), 'MSFT', 'NYSE', datetime(2021, 6, 1, 8, 0, 0, 0),
+                1, datetime(2021, 6, 1, 8, 30, 0, 0), Decimal(100.00)),
+               (date(2021, 6, 1), 'MSFT', 'NYSE', datetime(2021, 6, 1, 8, 0, 0, 0),
+                1, datetime(2021, 6, 1, 8, 45, 0, 0), Decimal(101.00)),
+               (date(2021, 6, 1), 'MSFT', 'NYSE', datetime(2021, 6, 1, 8, 0, 0, 0),
+                1, datetime(2021, 6, 1, 8, 35, 0, 0), Decimal(99.00)),
+               (date(2021, 6, 1), 'MSFT', 'NYSE', datetime(2021, 6, 1, 8, 0, 0, 0),
+                2, datetime(2021, 6, 1, 10, 30, 0, 0), Decimal(104.00)),
+               (date(2021, 6, 2), 'IBM', 'NYSE', datetime(2021, 6, 2, 8, 0, 0, 0),
+                1, datetime(2021, 6, 2, 8, 30, 0, 0), Decimal(150.00)),
+               (date(2021, 6, 2), 'IBM', 'NYSE', datetime(2021, 6, 2, 8, 0, 0, 0),
+                1, datetime(2021, 6, 2, 8, 40, 0, 0), Decimal(160.00))
+               )
+
+    latest_records = (
+        (date(2021, 6, 1), 'MSFT', 'NYSE', datetime(2021, 6, 1, 8, 0, 0, 0),
+         1, datetime(2021, 6, 1, 8, 45, 0, 0), Decimal(101.00)),
+        (date(2021, 6, 1), 'MSFT', 'NYSE', datetime(2021, 6, 1, 8, 0, 0, 0),
+         2, datetime(2021, 6, 1, 10, 30, 0, 0), Decimal(104.00)),
+        (date(2021, 6, 2), 'IBM', 'NYSE', datetime(2021, 6, 2, 8, 0, 0, 0),
+         1, datetime(2021, 6, 2, 8, 40, 0, 0), Decimal(160.00))
+    )
+
+    df = spark.createDataFrame(records, schema=TEST_SCHEMA)
+    df_applied = apply_latest(df)
+
+    assert set(df_applied.collect()) == set(latest_records)
