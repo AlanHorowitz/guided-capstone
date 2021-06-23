71c150ebe1af8073118f91a2bd3ace3aaa66812b
AlanHorowitz <alanhorowitz63@gmail.com>
2021-06-23 12:16:13 -0400

    deleting unneeded files

--- test.connection.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.master('local').appName('app').getOrCreate()
-
-
-sc = spark.sparkContext
-raw = sc.textFile("wasbs://test@guidedcapstonesa.blob.core.windows.net/part-00000-092ec1db-39ab-4079-9580-f7c7b516a283-c000.txt")
-raw.collect()
-print(raw.count())
