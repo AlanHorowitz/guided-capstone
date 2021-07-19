2a4c6cce8deba3f5beff86cc6bcc4b9192774e81
AlanHorowitz <alanhorowitz63@gmail.com>
2021-07-19 15:45:03 -0400

    initial step 5 commit

--- ingestion.py
+++ ingestion.py
@@ -16,15 +16,7 @@ import dateutil.parser
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField
 import pyspark.sql.types as T
-
-
-class Ingestion:
-    def __init__(self, config):
-        pass
-
-    def ingest(self):
-        pass
-
+import configparser
 
 QUOTE_COLUMNS = ('trade_dt', 'file_tm', 'event_type', 'symbol', 'event_tm', 'event_seq_nb',
                  'exchange', 'bid_pr', 'bid_size', 'ask_pr', 'ask_size')
@@ -58,196 +50,207 @@ input_files = [
 ]
 
 
-def get_input_files() -> str:
-    """Yield a URL for each file in the input directory"""
-    cloud_input_path = "wasbs://data@guidedcapstonesa.blob.core.windows.net/"
-    for filename in input_files:
-        yield cloud_input_path + filename
+class Ingestion:
+    def __init__(self, config):
+        parser = configparser.ConfigParser()
+        parser.read(config)
+        self._input_data_url = parser.get('app_config', 'input_data_url')
+        self._output_data_url = parser.get('app_config', 'output_data_url')
+        self._spark = SparkSession.builder.master('local').appName('guided-capstone').getOrCreate()
+
+    def ingest(self):
+        """
+        Guided Capstone data ingestion.  Read json and csv input files, transform data into a common schema
+        and persist in parquet format.
+        """
+        sc = self._spark.spark.sparkContext
+        all_data = spark.createDataFrame([], COMMON_SCHEMA)  # empty dataframe to accumulate all files
+
+        for file_location in get_input_files():
+            raw = sc.textFile(file_location)
+            if file_location[file_location.rfind('.'):] == '.json':
+                parser = parse_json
+            elif file_location[file_location.rfind('.'):] == '.csv':
+                parser = parse_csv
+            else:
+                print("Warning: unsupported file type detected")
+                continue
+            parsed = raw.map(parser)
+            data = spark.createDataFrame(parsed, schema=COMMON_SCHEMA)
+            all_data = all_data.union(data)
+
+        cloud_output_path = "wasbs://output@guidedcapstonesa.blob.core.windows.net/"
+        all_data.write.partitionBy('partition').mode('overwrite').parquet(cloud_output_path + 'stage')
 
 
-def map_column(col: str) -> str:
-    """Map column names used by quote and trade schemas to common schema names"""
-    return col if col not in COLUMN_MAPPINGS else COLUMN_MAPPINGS[col]
+    def get_input_files() -> str:
+        """Yield a URL for each file in the input directory"""
+        cloud_input_path = "wasbs://data@guidedcapstonesa.blob.core.windows.net/"
+        for filename in input_files:
+            yield cloud_input_path + filename
 
 
-def convert_value_to_common_type(value: Union[str, int, float], target_type: str) -> Union[str, int, Decimal, date, datetime]:
-    """
-    Convert a value to its required type. From csv, str is expected.  From json: str, int and float.
-
-    Args:
-       value: Value to be converted to the common type: string, int, and float are what the data currently present
-       target_type: target type as shown by dataFrame.printSchema() for COMMON_SCHEMA
-
-    Returns:
-       converted_value: value of target_type
-
-    Raises:
-       ValueError if any of the conversion functions fail.
-    """
-    if target_type == 'date':
-        converted_value = date.fromisoformat(value.strip())
-    elif target_type == 'timestamp':
-        converted_value = dateutil.parser.parse(value.strip())  # date.datetime.fromisoformat() too rigid
-    elif target_type == 'integer':
-        if isinstance(value, int):  # json
-            converted_value = int(value)
-        else:
-            converted_value = int(value.strip())
-    elif target_type.startswith('decimal'):
-        if isinstance(value, float):  # json
-            converted_value = Decimal.from_float(value)
-        else:
-            converted_value = Decimal(value.strip())
-    elif target_type == 'string':
-        converted_value = value.strip()
-
-    return converted_value
+    def map_column(col: str) -> str:
+        """Map column names used by quote and trade schemas to common schema names"""
+        return col if col not in COLUMN_MAPPINGS else COLUMN_MAPPINGS[col]
 
 
-def common_event(record_dict: Dict[str, Union[str, int, float]], schema: StructType, partition: str) -> List[Union[str, int, Decimal, date, datetime]]:
-    """
-    Create a format-validated record matching the supplied schema.
+    def convert_value_to_common_type(value: Union[str, int, float], target_type: str) -> Union[str, int, Decimal, date, datetime]:
+        """
+        Convert a value to its required type. From csv, str is expected.  From json: str, int and float.
 
-    Args:
-       record_dict: A dictionary of key/value pairs representing a quote or trade record
-       schema: the StructType of the target format
-       partition: A partition field to be added to the output record
+        Args:
+           value: Value to be converted to the common type: string, int, and float are what the data currently present
+           target_type: target type as shown by dataFrame.printSchema() for COMMON_SCHEMA
 
-    Returns:
-       common_event_row: List representing record in in schema format.
+        Returns:
+           converted_value: value of target_type
 
-    Raises:
-       ValueError if any of conversion function fails or a non-nullable field is missing from the record.
-    """
+        Raises:
+           ValueError if any of the conversion functions fail.
+        """
+        if target_type == 'date':
+            converted_value = date.fromisoformat(value.strip())
+        elif target_type == 'timestamp':
+            converted_value = dateutil.parser.parse(value.strip())  # date.datetime.fromisoformat() too rigid
+        elif target_type == 'integer':
+            if isinstance(value, int):  # json
+                converted_value = int(value)
+            else:
+                converted_value = int(value.strip())
+        elif target_type.startswith('decimal'):
+            if isinstance(value, float):  # json
+                converted_value = Decimal.from_float(value)
+            else:
+                converted_value = Decimal(value.strip())
+        elif target_type == 'string':
+            converted_value = value.strip()
 
-    common_event_row = []
-    try:
-        # visit the target schema fields in order
+        return converted_value
+
+
+    def common_event(record_dict: Dict[str, Union[str, int, float]], schema: StructType, partition: str) -> List[Union[str, int, Decimal, date, datetime]]:
+        """
+        Create a format-validated record matching the supplied schema.
+
+        Args:
+           record_dict: A dictionary of key/value pairs representing a quote or trade record
+           schema: the StructType of the target format
+           partition: A partition field to be added to the output record
+
+        Returns:
+           common_event_row: List representing record in in schema format.
+
+        Raises:
+           ValueError if any of conversion function fails or a non-nullable field is missing from the record.
+        """
+
+        common_event_row = []
+        try:
+            # visit the target schema fields in order
+            for struct_field in schema:
+                field_name = struct_field.jsonValue()['name']
+                field_type = struct_field.jsonValue()['type']
+                field_nullable = struct_field.jsonValue()['nullable']
+
+                if field_name == 'partition':
+                    value = partition
+                elif field_name in record_dict:
+                    value = convert_value_to_common_type(record_dict[field_name], field_type)
+                elif field_nullable:
+                    value = None
+                else:
+                    raise ValueError(f'NonNullable field missing from record {record_dict}')
+                common_event_row.append(value)
+
+            return common_event_row
+
+        except Exception:
+            raise ValueError(f'Value conversion error from record {record_dict}')
+
+
+    def error_event(schema: StructType, partition: str, line: str) -> List[Union[str, int, Decimal, date, datetime]]:
+        """
+        Create a format-validated error record matching the supplied schema.
+
+        Args:
+           schema: the StructType of the target format
+           partition: A partition field to be added to the output record
+           line: The raw text of the record that wasn't successfully parsed
+
+        Returns:
+           error_event_row: List representing error record in schema format. Hardcoded error values are used for data fields.
+        """
+        error_event_row = []
         for struct_field in schema:
             field_name = struct_field.jsonValue()['name']
             field_type = struct_field.jsonValue()['type']
-            field_nullable = struct_field.jsonValue()['nullable']
 
             if field_name == 'partition':
                 value = partition
-            elif field_name in record_dict:
-                value = convert_value_to_common_type(record_dict[field_name], field_type)
-            elif field_nullable:
-                value = None
+            elif field_name == 'line':
+                value = line
+            elif field_type == 'date':
+                value = date(2000, 1, 1)
+            elif field_type == 'timestamp':
+                value = datetime(2000, 1, 1, 0, 0, 0, 0)
+            elif field_type == 'integer':
+                value = -1
+            elif field_type.startswith('decimal'):
+                value = Decimal(-1.0)
+            elif field_type == 'string':
+                value = 'INVALID'
+
+            error_event_row.append(value)
+
+        return error_event_row
+
+
+    def parse_csv(line: str) -> List[Union[str, int, Decimal, date, datetime]]:
+        """
+        Parse a line of text in csv format, setting partition column as record type.
+
+        :param line: A line of comma delimited text
+        :return: Record in format of COMMON_SCHEMA. May be successfully parsed data or Error record.
+        """
+        try:
+            record_type_pos = 2
+            record = line.split(',')
+            record_type = record[record_type_pos]
+
+            if record_type == 'Q' and len(record) == len(QUOTE_COLUMNS):
+                record_dict = {map_column(c): record[i] for i, c in enumerate(QUOTE_COLUMNS)}
+                return common_event(record_dict, COMMON_SCHEMA, 'Q')
+            elif record_type == 'T' and len(record) == len(TRADE_COLUMNS):
+                record_dict = {map_column(c): record[i] for i, c in enumerate(TRADE_COLUMNS)}
+                return common_event(record_dict, COMMON_SCHEMA, 'T')
             else:
-                raise ValueError(f'NonNullable field missing from record {record_dict}')
-            common_event_row.append(value)
+                raise ValueError(f'Unknown record type or incorrect number of fields from record {record}')
 
-        return common_event_row
-
-    except Exception:
-        raise ValueError(f'Value conversion error from record {record_dict}')
+        except ValueError:
+            return error_event(COMMON_SCHEMA, 'B', line)
 
 
-def error_event(schema: StructType, partition: str, line: str) -> List[Union[str, int, Decimal, date, datetime]]:
-    """
-    Create a format-validated error record matching the supplied schema.
+    def parse_json(line: str) -> List[Union[str, int, Decimal, date, datetime]]:
+        """
+        Parse a line of text in json format, setting partition column as record type.
 
-    Args:
-       schema: the StructType of the target format
-       partition: A partition field to be added to the output record
-       line: The raw text of the record that wasn't successfully parsed
+        :param line: A line of one json record
+        :return: Record in format of COMMON_SCHEMA. May be successfully parsed data or Error record.
+        """
+        try:
+            record = json.loads(line)
+            record_type = record['event_type']
 
-    Returns:
-       error_event_row: List representing error record in schema format. Hardcoded error values are used for data fields.
-    """
-    error_event_row = []
-    for struct_field in schema:
-        field_name = struct_field.jsonValue()['name']
-        field_type = struct_field.jsonValue()['type']
+            if record_type == 'Q' or record_type == 'T':
+                translated_record = {map_column(k): v for k, v in record.items()}
+                return common_event(translated_record, COMMON_SCHEMA, record_type)
+            else:
+                raise ValueError(f'Unknown record type from record {record}')
 
-        if field_name == 'partition':
-            value = partition
-        elif field_name == 'line':
-            value = line
-        elif field_type == 'date':
-            value = date(2000, 1, 1)
-        elif field_type == 'timestamp':
-            value = datetime(2000, 1, 1, 0, 0, 0, 0)
-        elif field_type == 'integer':
-            value = -1
-        elif field_type.startswith('decimal'):
-            value = Decimal(-1.0)
-        elif field_type == 'string':
-            value = 'INVALID'
-
-        error_event_row.append(value)
-
-    return error_event_row
+        except ValueError:
+            return error_event(COMMON_SCHEMA, 'B', line)
 
 
-def parse_csv(line: str) -> List[Union[str, int, Decimal, date, datetime]]:
-    """
-    Parse a line of text in csv format, setting partition column as record type.
-
-    :param line: A line of comma delimited text
-    :return: Record in format of COMMON_SCHEMA. May be successfully parsed data or Error record.
-    """
-    try:
-        record_type_pos = 2
-        record = line.split(',')
-        record_type = record[record_type_pos]
-
-        if record_type == 'Q' and len(record) == len(QUOTE_COLUMNS):
-            record_dict = {map_column(c): record[i] for i, c in enumerate(QUOTE_COLUMNS)}
-            return common_event(record_dict, COMMON_SCHEMA, 'Q')
-        elif record_type == 'T' and len(record) == len(TRADE_COLUMNS):
-            record_dict = {map_column(c): record[i] for i, c in enumerate(TRADE_COLUMNS)}
-            return common_event(record_dict, COMMON_SCHEMA, 'T')
-        else:
-            raise ValueError(f'Unknown record type or incorrect number of fields from record {record}')
-
-    except ValueError:
-        return error_event(COMMON_SCHEMA, 'B', line)
 
 
-def parse_json(line: str) -> List[Union[str, int, Decimal, date, datetime]]:
-    """
-    Parse a line of text in json format, setting partition column as record type.
-
-    :param line: A line of one json record
-    :return: Record in format of COMMON_SCHEMA. May be successfully parsed data or Error record.
-    """
-    try:
-        record = json.loads(line)
-        record_type = record['event_type']
-
-        if record_type == 'Q' or record_type == 'T':
-            translated_record = {map_column(k): v for k, v in record.items()}
-            return common_event(translated_record, COMMON_SCHEMA, record_type)
-        else:
-            raise ValueError(f'Unknown record type from record {record}')
-
-    except ValueError:
-        return error_event(COMMON_SCHEMA, 'B', line)
-
-
-if __name__ == '__main__':
-    """
-    Guided Capstone data ingestion.  Read json and csv input files, transform data into a common schema
-    and persist in parquet format. 
-    """
-    spark = SparkSession.builder.master('local').appName('app').getOrCreate()
-    sc = spark.sparkContext
-    all_data = spark.createDataFrame([], COMMON_SCHEMA)  # empty dataframe to accumulate all files
-
-    for file_location in get_input_files():
-        raw = sc.textFile(file_location)
-        if file_location[file_location.rfind('.'):] == '.json':
-            parser = parse_json
-        elif file_location[file_location.rfind('.'):] == '.csv':
-            parser = parse_csv
-        else:
-            print("Warning: unsupported file type detected")
-            continue
-        parsed = raw.map(parser)
-        data = spark.createDataFrame(parsed, schema=COMMON_SCHEMA)
-        all_data = all_data.union(data)
-
-    cloud_output_path = "wasbs://output@guidedcapstonesa.blob.core.windows.net/"
-    all_data.write.partitionBy('partition').mode('overwrite').parquet(cloud_output_path + 'stage')
--- tracker.py
+++ tracker.py
@@ -1,2 +1,14 @@
 class Tracker:
+
     def __init__(self, name, config):
+        self._name = name
+        self._config = config
+
+    def update_job_status(self):
+        pass
+
+    def assign_job_id(self):
+        pass
+
+    def get_job_status(self):
+        pass
