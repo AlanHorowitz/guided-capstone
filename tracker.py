8455ee35702a2743c32351079f1bce254c9930fb
AlanHorowitz <alanhorowitz63@gmail.com>
2021-07-20 15:17:22 -0400

    more stuff

--- tracker.py
+++ tracker.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import psycopg2
 from psycopg2.extensions import connection, cursor
 from configparser import ConfigParser
@@ -16,11 +17,11 @@ class Tracker:
         job_id = self._assign_job_id()
         update_time = datetime.now()
         if self._connection is not None:
-            insert_string = "INSERT into %s (job_id, update_time, status) VALUES (%s,%s,%s);"
+            insert_string = f"INSERT into {self._config.get('PRODUCTION', 'TrackingTableName')}" + \
+                            " (job_id, update_time, status) VALUES (%s,%s,%s);"
             try:
                 cur: cursor = self._connection.cursor()
-                cur.execute(insert_string, self._config.get('PRODUCTION', 'TrackingTableName'),
-                            job_id, update_time, status)
+                cur.execute(insert_string, (job_id, update_time, status))
                 self._connection.commit()
                 cur.close()
             except psycopg2.Error as e:
@@ -31,26 +32,30 @@ class Tracker:
     def _assign_job_id(self) -> str:
         return self._name + self._trade_date
 
-    def get_job_status(self, job_id: str):
+    def get_job_status(self, job_id: str) -> Tuple:
         if self._connection is not None:
-            query_string = "SELECT job_id, update_time, status FROM %s WHERE job_id = %s;"
+            query_string = f"SELECT job_id, update_time, status FROM {self._config.get('PRODUCTION', 'TrackingTableName')}" + \
+                           " WHERE job_id = %s;"
             try:
                 cur: cursor = self._connection.cursor()
-                result = cur.execute(query_string, self._config.get('PRODUCTION', 'TrackingTableName'), job_id)
+                cur.execute(query_string, (job_id,))
+                result = cur.fetchone()
                 self._connection.commit()
                 cur.close()
                 return result
             except psycopg2.Error as e:
                 print("Error reading tracking table: ", e.pgerror)
+                return None
         else:
             print("Error: Connection to tracking table not available")
+            return None
 
     def _get_db_connection(self):
         try:
-            conn: connection = psycopg2.connect(database=self._config.get('DATABASE', 'Database'),
-                                                host=self._config.get('DATABASE', 'Host'),
-                                                user=self._config.get('DATABASE', 'User'),
-                                                password=self._config.get('DATABASE', 'Password')
+            conn: connection = psycopg2.connect(database=self._config.get('PRODUCTION', 'Database'),
+                                                host=self._config.get('PRODUCTION', 'Host'),
+                                                user=self._config.get('PRODUCTION', 'User'),
+                                                password=self._config.get('PRODUCTION', 'Password')
                                                 )
             return conn
         except psycopg2.Error as e:
